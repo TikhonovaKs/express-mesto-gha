889981769db8f0a00a68ad2af7bea58a7e9eb676
Kseniia Tikhonova <tikhonovakb@gmail.com>
2023-06-29 21:19:09 +0200

    fixed unauth issue

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,5 @@
 const errorHandler = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
-
   // res.status(statusCode).send({ message: /*statusCode === 500 ? 'Server error' :*/ message });
   res.status(statusCode).send({ message });
 
